e6c71a074999cf537c1bd8a9b06062cb65e2cedb
Zhabelle <dmunoz024@gmail.com>
2023-05-03 20:03:36 -0600

    Reimplementation of minigames to first scenes

--- Assets/Fungus/Scripts/Minigames/Fish/BarColoreador.cs
+++ Assets/Fungus/Scripts/Minigames/Fish/BarColoreador.cs
@@ -6,14 +6,23 @@ using Fungus;
 public class BarColoreador : MonoBehaviour{
     [SerializeField] Flowchart flowchart;
     SpriteRenderer sprite;
+    Collider2D col;
+    FloatVariable hueVar;
 
     private void Start() {
         sprite = GetComponent<SpriteRenderer>();
+        col = GetComponent<Collider2D>();
+        hueVar = flowchart.GetVariable<FloatVariable>("fishingTiem");
     }
 
-    // Update is called once per frame
+    // Change the bar color based on the remaining time to catch the fish
     void FixedUpdate(){
-        float hue = flowchart.GetVariable<FloatVariable>("fishingTiem").Value;
-        sprite.color = Color.HSVToRGB(hue/360.0f,0.73f,0.97f);
+        sprite.color = Color.HSVToRGB(hueVar.Value/360.0f,0.73f,0.97f);
+    }
+
+    // Scene compatibility assertion
+    private void OnCollisionEnter2D(UnityEngine.Collision2D collision) {
+        if(!collision.gameObject.CompareTag("Laser"))
+            Physics2D.IgnoreCollision(collision.collider, col);
     }
 }
--- Assets/Fungus/Scripts/Minigames/ImageTilesPuzzle/SlidingPuzzleTile.cs
+++ Assets/Fungus/Scripts/Minigames/ImageTilesPuzzle/SlidingPuzzleTile.cs
@@ -3,56 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using Fungus;
 
-// public class SlidingPuzzleTile : MonoBehaviour{
-//     [SerializeField]
-//     SlidingPuzzleBoard board;
-//     [SerializeField]
-//     float rayDistance;
-//     [SerializeField]
-//     Flowchart flowchart;
-//     Vector2 targetPos;
-//     [SerializeField]
-//     LayerMask layerMask;
-
-//     private void OnMouseOver() {
-//         if(Input.GetMouseButton(0) && !board.isTileMoving) {
-//             Vector2[] directions = new Vector2[4] {Vector2.left, Vector2.up, Vector2.right, Vector2.down};
-//             Vector2 openDir = Vector2.negativeInfinity;
-//             foreach(Vector2 dir in directions) {
-//                 Vector2 startPos = new Vector2(transform.position.x + dir.x * transform.localScale.x / 1.5f, transform.position.y + dir.y * transform.localScale.y / 1.5f);
-//                 if(!Physics2D.Raycast(startPos, dir, rayDistance, layerMask)) {
-//                     Debug.DrawRay(startPos, dir, Color.cyan, rayDistance);
-//                     openDir = dir;
-//                     break;
-//                 }else {
-//                     Debug.DrawRay(startPos, dir, Color.green, rayDistance);
-//                 }
-//             }
-//             if(openDir.Equals(Vector2.negativeInfinity)) {
-//                 if(!flowchart.FindBlock("TileShakeBlock").IsExecuting() || !flowchart.GetVariable<GameObjectVariable>("currTile").Value.Equals(gameObject)) {
-//                     flowchart.GetVariable<GameObjectVariable>("currTile").Value = gameObject;
-//                     flowchart.ExecuteBlock("TileShakeBlock");
-//                 }
-//                 return;
-//             }
-//             board.isTileMoving = true;
-//             flowchart.GetVariable<GameObjectVariable>("currTile").Value = gameObject;
-//             flowchart.GetVariable<Vector3Variable>("tileDest").Value = new Vector3(transform.localPosition.x + openDir.x * board.spacing, transform.localPosition.y + openDir.y * board.spacing);
-//             flowchart.ExecuteBlock("TileMoveBlock");
-//         }
-//     }
-
-//     public void Win() => flowchart.ExecuteBlock("PuzzleWonBlock");
-
-//     public Vector2 GetTargetPos() => targetPos;
-//     public void SetTargetPos(Vector2 targetPos) => this.targetPos = targetPos;
-
-//     public bool IsOnTargetPos() {
-//         return transform.localPosition.Equals(targetPos);
-//     }
-// }
-
-
 /// <summary>
 /// Este script de Unity es para cada pieza (tile) en el juego de rompecabezas deslizante.
 /// Define el comportamiento de las piezas y la lógica de movimiento en el tablero.
@@ -78,7 +28,33 @@ public class SlidingPuzzleTile : MonoBehaviour
     /// </summary>
     private void OnMouseOver()
     {
-        // ...
+        if(!Input.GetMouseButton(0) || board.isTileMoving) return;
+        Vector2[] directions = new Vector2[4] {Vector2.left, Vector2.up, Vector2.right, Vector2.down};
+        Vector2 openDir = Vector2.negativeInfinity;
+        // Verificar si la pieza puede moverse
+        foreach(Vector2 dir in directions) {
+            Vector2 startPos = new Vector2(transform.position.x + dir.x * transform.localScale.x / 1.5f, transform.position.y + dir.y * transform.localScale.y / 1.5f);
+            if(!Physics2D.Raycast(startPos, dir, rayDistance, layerMask)) {
+                Debug.DrawRay(startPos, dir, Color.cyan, rayDistance);
+                openDir = dir;
+                break;
+            }else {
+                Debug.DrawRay(startPos, dir, Color.green, rayDistance);
+            }
+        }
+        // Si la pieza no puede moverse, agitarla y salir
+        if(openDir.Equals(Vector2.negativeInfinity)) {
+            if(!flowchart.FindBlock("TileShakeBlock").IsExecuting() || !flowchart.GetVariable<GameObjectVariable>("currTile").Value.Equals(gameObject)) {
+                flowchart.GetVariable<GameObjectVariable>("currTile").Value = gameObject;
+                flowchart.ExecuteBlock("TileShakeBlock");
+            }
+            return;
+        }
+        // Mover la pieza a su lugar
+        board.isTileMoving = true;
+        flowchart.GetVariable<GameObjectVariable>("currTile").Value = gameObject;
+        flowchart.GetVariable<Vector3Variable>("tileDest").Value = new Vector3(transform.localPosition.x + openDir.x * board.spacing, transform.localPosition.y + openDir.y * board.spacing);
+        flowchart.ExecuteBlock("TileMoveBlock");
     }
 
     /// <summary>
@@ -102,8 +78,5 @@ public class SlidingPuzzleTile : MonoBehaviour
     /// Verifica si la pieza está en su posición objetivo.
     /// </summary>
     /// <returns>True si la pieza está en su posición objetivo, de lo contrario, False.</returns>
-    public bool IsOnTargetPos()
-    {
-        return transform.localPosition.Equals(targetPos);
-    }
+    public bool IsOnTargetPos() => transform.localPosition.Equals(targetPos);
 }
