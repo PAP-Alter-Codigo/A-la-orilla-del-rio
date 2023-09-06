08520149b37f7e12eca6e7ee78da9fea1a75664c
Zhabelle <dmunoz024@gmail.com>
2023-09-06 17:02:19 -0600

    Initial commit
    Ahora puedo seguir desde mi pcerda

---
+++ Assets/Scripts/Minigames/TheCoolerFish/FishingMinigameV2.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FishingMinigameV2 : MonoBehaviour {
+    public Sprite[] fishies;
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
