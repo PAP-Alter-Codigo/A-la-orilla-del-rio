fc3db230b9ab3579d24c90daa9dff8bb3d0bcda1
Gustavo Sotres <gus.sotres@gmail.com>
2023-09-06 09:14:37 -0600

    Preliminar Inventory Sistem

---
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryManager : MonoBehaviour
+{
+	[SerializeField]
+	private Image[] inventorySlots = new Image[4];
+	
+	private InventoryItem[] items = new InventoryItem[4];
+	private uint inventoryIndex = 0;
+
+
+	public void addItem(InventoryItem newItem)
+	{
+		if(inventoryIndex >= 3) return;
+
+		items[inventoryIndex] = newItem;
+		inventoryIndex++;
+
+		updateUI();
+	}
+
+	public void clearInventory()
+	{
+		for (int i = 0; i < items.Length; i++)
+		{
+			items[i] = null;
+		}
+		inventoryIndex = 0;
+
+		for (int i = 0; i < 4; i++)
+		{
+			inventorySlots[i].sprite = null;
+		}
+
+	}
+
+
+	private void updateUI()
+	{
+		// 4 Es el numero maximo de items que guardamos
+		print("Llegue aqui");
+		for (int i = 0; i < inventoryIndex; i++)
+		{
+			inventorySlots[i].sprite = items[i].itemSprite;
+		}
+		print("Llegue aqui");
+	}
+
+}
+
+
+
+
