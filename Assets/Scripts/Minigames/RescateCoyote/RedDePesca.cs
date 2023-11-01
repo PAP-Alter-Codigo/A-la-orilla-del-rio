fad3bf360eec88c669498a7633a2614c59022f42
Zhabelle <dmunoz024@gmail.com>
2023-11-01 11:51:48 -0600

    Art assets

--- Assets/Scripts/Minigames/RescateCoyote/RedDePesca.cs
+++ Assets/Scripts/Minigames/RescateCoyote/RedDePesca.cs
@@ -8,7 +8,8 @@ public class RedDePesca : MonoBehaviour
 	private Collider2D redColider;
 	[SerializeField]
 	private Collider2D coyoteColider;
-
+	[SerializeField]
+	private Vector3 offset;
 	[SerializeField]
 	private float recoverySpeed;
 	[SerializeField]
@@ -23,7 +24,7 @@ public class RedDePesca : MonoBehaviour
             Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 			transform.position = new Vector3(mousePosition.x, mousePosition.y, 0.0f);
         }else {
-			transform.position = Vector2.MoveTowards(transform.position, recoveryPoint.position, recoverySpeed * Time.deltaTime);
+			transform.position = Vector2.MoveTowards(transform.position, recoveryPoint.position + offset, recoverySpeed * Time.deltaTime);
 		}
 		ProgressBar.MakingProgress = redColider.IsTouching(coyoteColider);
 
