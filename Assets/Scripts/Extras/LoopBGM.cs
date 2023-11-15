e593e95ed624a2f026061fbc223b95316c47c788
Gwendeline Orozco <gwen_orozco@outlook.com>
2023-11-15 12:34:18 -0600

    updated music in intro and tutorial

---
+++ Assets/Scripts/Extras/LoopBGM.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(AudioSource))]
+public class LoopBGM : MonoBehaviour
+{
+public AudioClip StartClip;
+public AudioClip LoopClip;
+
+    void Start()
+    {
+        StartCoroutine(playSound());
+    }
+
+    IEnumerator playSound()
+    {
+        GetComponent<AudioSource>().clip = StartClip;
+        GetComponent<AudioSource>().Play();
+        yield return new WaitForSeconds(StartClip.length);
+        GetComponent<AudioSource>().clip = LoopClip;
+        GetComponent<AudioSource>().Play();
+        GetComponent<AudioSource>().loop = true;
+    }
+}
